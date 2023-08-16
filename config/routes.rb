a2c6365fb5345d73dba1070a514faa103d9b0408
Feliverse <haybar@gmail.com>
2023-08-16 15:36:27 -0400

    Generate greetings table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'root#index'
   
   get 'root/index'
+
+  get 'greetings', to: 'greetings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "¡Hi!")
+Greeting.create(message: "¡Hello, How are you?")
+Greeting.create(message: "Hi from my React on Rails App")
+Greeting.create(message: "¡Hello!")
+Greeting.create(message: "¡Hi hi naranhi")
+Greeting.create(message: "Nice to see you again")
