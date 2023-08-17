fe50fc43c748f6f14cd55e0b0d58b93a6bee90a5
Feliverse <haybar@gmail.com>
2023-08-16 20:17:34 -0400

    Install Rubocop to enforce Rails code

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -2,6 +2,7 @@ class GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
   end
+
   def random
     @random_greeting = Greeting.order('RANDOM()').first
     render json: @random_greeting
